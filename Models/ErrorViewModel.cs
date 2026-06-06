b8543ec96147c52984f2d4746d0a39c7bce1f03b
AnndyMadrigal <149017080+AnndyMadrigal@users.noreply.github.com>
2026-06-06 10:27:32 -0600

    Creacio de Repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PAW_Proyecto_Kronos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
